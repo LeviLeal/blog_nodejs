3a0f6d8f5e4a2cc606c16d512e60e76bf2c27372
LeviLeal <hunt3rgz@protonmail.com>
2023-10-24 18:38:53 -0300

    [+Adjustment] Ajuste para deploy na AWS

--- app.js
+++ app.js
@@ -146,8 +146,7 @@ app.use("/usuarios", usuarios)
 
 // EXPRESS PARA RODAR E ESCUTAR A PORTA DEFINIDA
 
-const PORT = "8081"
-
+const PORT = process.env.PORT || "8081"
 app.listen(PORT, () => {
     console.log("== Server rodando ==")
 })
